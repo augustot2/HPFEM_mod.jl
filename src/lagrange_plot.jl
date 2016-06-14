bd6edd832c95b0d28691f06c84f8b62e7f9aaf39
Augusto <augustot2@gmail.com>
2016-06-14 15:27:07 -0300

    neumann

--- src/lagrange_plot.jl
+++ src/lagrange_plot.jl
@@ -124,3 +124,69 @@ function lagrange_aprox_plot(M,Q,nel,fun,a=-1,b=1,idir=[1,nel],nξ = 101)
     return 0
 
 end
+
+
+
+function lagrange_oed_neum_plot(M,Q,nel,rhsfun,uexact,duexact,a=-1,b=1,idir=[1,nel],nξ = 101)
+  TF = Float64
+  nnodes = nel + 1
+  b1 = HPFEM.Lagrange1d(M, TF)
+  quad = HPFEM.QuadType(Q, HPFEM.GLJ, TF)
+  bas = HPFEM.Basis1d(b1,quad, TF)
+  lmap = HPFEM.locmap(bas)
+  dof = HPFEM.DofMap1d(lmap, nnodes, idir);
+
+  nodes = [TF(x) for x in linspace(a, b, nnodes)];
+
+  elems = [HPFEM.Element1d(e, nodes[e], nodes[e+1], bas) for e = 1:nel];
+
+  solver = HPFEM.CholeskySC(dof, HPFEM.BBMatrix1d, TF);
+
+
+
+  for e = 1:nel
+      Ae = zeros(TF, M, M)
+      HPFEM.add_stiff_matrix!(bas, elems[e], Ae)
+      HPFEM.add_mass_matrix!(bas, elems[e], Ae)
+      HPFEM.add_local_matrix(solver, e, Ae)
+  end
+
+
+  solver.Abb
+
+  Fe = zeros(HPFEM.nmodes(lmap), nel)
+  bnd = HPFEM.bndidx(lmap)
+
+  for e = 1:nel
+      fe = rhsfun(elems[e].x)
+      HPFEM.add_rhs!(bas, elems[e], fe, sub(Fe, :, e))
+  end
+  Fe[bnd[2],nel] += bas(one(TF), bnd[2]) * duexact(b) ### A: Adicionar o termo vdu/dx no lado direito!
+  # Apply Dirichilet BCs:
+  Fe[bnd[1],1] = uexact(a);
+
+  HPFEM.solve!(solver, Fe);
+
+  nξ = 101
+  ξ = collect(linspace(-1,1,nξ));
+  ϕ = zeros(nξ, M)
+  for i = 1:M
+      ϕ[:,i] = bas(ξ, i)
+  end
+
+  Ue = ϕ * Fe;
+
+  #plot
+  maxerr = 0.0
+  for e = 1:nel
+      el = elems[e]
+      x = (1-ξ)*el.a/2 + (1+ξ)*el.b/2
+      uu = uexact(x)
+      err = maxabs(uu-Ue[:,e])
+      if err > maxerr maxerr = err end
+
+      plot(x, Ue[:,e], "r", x, uu, "b")
+  end
+  return maxerr
+
+end
